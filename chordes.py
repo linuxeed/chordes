68673e5dbc429b698d9c51464e5cbaef401be7d8
linuxeed <linuxeed@gmail.com>
2023-06-27 21:14:08 +0300

    Alignment using HTML <table> tag

--- chordes.py
+++ chordes.py
@@ -1,50 +1,62 @@
 import sys
 
+NOTES = "abcdefgh"
 def getChordURL(chord):
 	res = 'https://tuneronline.ru/images/chords/guitar/max/'
 	
 	for char in chord:
 		char = char.lower()
-		if char == '#':
+		if char == '#' or char == '♯':
 			res += 'diez'
+		elif char == 'b' or char == '♭':
+			res += 'b'
 		else:
 			res += char
 	
 	res += '.jpg'
 	return res
 
-notes = "abcdefgh"
-
-####################################################
-
 if len(sys.argv) <= 2:
 	print('usage: song.txt song.html')
-	exit()
+	exit(0)
 
 fin  = open(sys.argv[1], 'r')
 fout = open(sys.argv[2], 'w')
 
-print('<meta charset="utf-8">', file=fout) # fix charset
-print('<font face="sans-serif" size="3">', file=fout) # sans font
+print('<meta charset="utf-8">', file=fout)
+print('<font face="sans-serif" size="3">', file=fout)
+print('<table>', file=fout)
 
 for line in fin:
+	print('<tr><td>', file=fout)
+	
+	for word in line.split():
+		brackets = word[0] == '(' and word[-1] == ')'
+		if brackets:
+			if not word[1].lower() in NOTES:
+				print(word, file=fout)
+		else:
+			if not word[0].lower() in NOTES:
+				print(word, file=fout)
+	
+	print('</td><td>', file=fout)
+	
 	for word in line.split():
 		brackets = word[0] == '(' and word[-1] == ')'
 		if brackets:
 			word = word[1:-2]
 			print('(', file=fout)
 		
-		if word[0].lower() in notes:
+		if word[0].lower() in NOTES:
 			print(f'<img src="{getChordURL(word)}" height="40">', file=fout)
-		else:
-			print(word, file=fout)
 		
 		if brackets:
 			print(')', file=fout)
 	
-	print('<br>', file=fout)
+	print('</td></tr>', file=fout)
 
-print('</font>', file=fout) # end sans font
+print('</font>', file=fout)
+print('</table>', file=fout)
 
 fin.close()
 fout.close()
