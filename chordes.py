5e79f84df3616fdf6e9ae286273689df6b07eaab
linuxeed <linuxeed@gmail.com>
2023-07-16 13:57:16 +0300

    some useful changes

--- chordes.py
+++ chordes.py
@@ -15,6 +15,10 @@ def get_chord_url(chord):
 	return f'https://tuneronline.ru/images/chords/guitar/max/{res}.jpg'
 
 
+def big_font(x):
+	return f'<font face="sans-serif" size="5">{x}</font>'
+
+
 if len(sys.argv) <= 2:
 	print('usage: song.txt song.html')
 	exit(0)
@@ -23,37 +27,33 @@ fin = open(sys.argv[1], 'r')
 fout = open(sys.argv[2], 'w')
 
 print('<meta charset="utf-8">', file=fout)
-print('<font face="sans-serif" size="3">', file=fout)
-print('<table>', file=fout)
 
+print('<table>', file=fout)
 for line in fin:
-	print('<tr><td>', file=fout)
+	print('<tr>\n<td>', file=fout)
 	
 	for word in line.split():
-		if word[0] == '(' and word[-1] == ')':
-			if not word[1].lower() in NOTES:
-				print(word, file=fout)
-		else:
-			if not word[0].lower() in NOTES:
-				print(word, file=fout)
+		brackets = word[0] == '(' and word[-1] == ')'
+		if (brackets and word[1].lower() not in NOTES) or (not brackets and word[0].lower() not in NOTES):
+			print(big_font(word), file=fout)
 	
-	print('</td><td>', file=fout)
+	print('</td>\n<td>', file=fout)
 	
 	for word in line.split():
 		brackets = word[0] == '(' and word[-1] == ')'
 		if brackets:
 			word = word[1:-1]
-			print('(', file=fout)
+			print(big_font('('), file=fout)
 		
 		if word[0].lower() in NOTES:
-			print(f'<img src="{get_chord_url(word)}" height="40">', file=fout)
+			print(f'<img src="{get_chord_url(word)}" height="90">', file=fout)
 		
 		if brackets:
-			print(')', file=fout)
+			print(big_font(')'), file=fout)
 	
-	print('</td></tr>', file=fout)
-
+	print('</td>\n</tr>', file=fout)
 print('</table>', file=fout)
+
 print('</font>', file=fout)
 
 fin.close()
